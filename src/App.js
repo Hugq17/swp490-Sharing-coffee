544e6fbcf151046a89735a5f6865f1896622db4d
hung1720 <coderlazy17@gmail.com>
2024-01-15 16:52:48 +0700

    update page login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import AuthLogin from './pages/common/auth-login/page';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to="/login" />}></Route>
+        <Route path='/login' element={<AuthLogin />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
