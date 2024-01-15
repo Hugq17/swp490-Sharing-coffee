e76cd5ee59f32ba7a9ee6e067adc277b368de6bb
hung1720 <coderlazy17@gmail.com>
2024-01-15 17:12:48 +0700

    delete img

--- src/pages/common/auth-login/page.js
+++ src/pages/common/auth-login/page.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 const page = () => {
     return (
         <>
-            <section className="md:h-screen py-36 flex items-center  bg-no-repeat bg-center bg-cover" style={{ backgroundImage: "url('/images/cta.jpg')" }}>
+            <section className="md:h-screen py-36 flex justify-center items-center  bg-no-repeat bg-center bg-cover" style={{ backgroundImage: "url('/images/cta.jpg')" }}>
                 <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black"></div>
                 <div className="container relative">
                     <div className="flex justify-center">
                         <div className="max-w-[400px] w-full m-auto p-6 bg-white dark:bg-slate-900 shadow-md dark:shadow-gray-800 rounded-md">
-                            {/* <Link href="/"><img src="../../images/logo-icon-64.png" height={64} width={72} className="mx-auto" alt="" /></Link> */}
+                            <img src="../../images/logo-icon-64.png" height={64} width={72} className="mx-auto" alt="" />
                             <h5 className="my-6 text-xl font-semibold">Login</h5>
                             <form className="text-start">
                                 <div className="grid grid-cols-1">
