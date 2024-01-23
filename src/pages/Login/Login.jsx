cd3d6060ff21e075b5c2ba33e8d9494a48f184a1
hung1720 <coderlazy17@gmail.com>
2024-01-23 10:59:05 +0700

    update login admin

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import React from "react"
 // import Link from "next/link";
 // import Image from "next/image";
 import logo from '../../assets/images/logo-icon-30.png'
+import { Player } from '@lottiefiles/react-lottie-player';
 // import Switcher from "../components/switcher";
 // import BackButton from "../components/backButton";
 
@@ -16,7 +17,13 @@ export default function Login() {
                         <div className="xl:w-[30%] lg:w-1/3 md:w-1/2">
                             <div className="relative md:flex flex-col md:min-h-screen justify-center bg-white dark:bg-slate-900 shadow dark:shadow-gray-700 md:px-10 py-10 px-4 z-1">
                                 <div className="text-center">
-                                    <img src={logo} width={72} height={64} placeholder="blur" className="mx-auto" alt="" />
+                                    {/* <img src={logo} width={72} height={64} placeholder="blur" className="mx-auto" alt="" /> */}
+                                    <Player
+                                        src='https://lottie.host/39aafee5-fbbb-4f97-8a30-0f02808e558a/h6pcKjMDMI.json'
+                                        className="player"
+                                        loop
+                                        autoplay
+                                    />
                                 </div>
                                 <div className="title-heading text-center md:my-auto my-20">
                                     <form className="text-start">
@@ -31,22 +38,10 @@ export default function Login() {
                                                 <input id="LoginPassword" type="password" className="form-input mt-3 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0" placeholder="Password:" />
                                             </div>
 
-                                            <div className="flex justify-between mb-4">
-                                                <div className="flex items-center mb-0">
-                                                    <input className="form-checkbox rounded border-gray-200 dark:border-gray-800 text-indigo-600 focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50 me-2" type="checkbox" value="" id="RememberMe" />
-                                                    <label className="form-checkbox-label text-slate-400" htmlFor="RememberMe">Remember me</label>
-                                                </div>
-                                                <p className="text-slate-400 mb-0">
-                                                    {/* <Link href="/auth-re-password" className="text-slate-400">Forgot password ?</Link> */}
-                                                </p>
-                                            </div>
+
 
                                             <div className="mb-4">
-                                                <input type="submit" className="py-2 px-5 inline-block tracking-wide border align-middle duration-500 text-base text-center bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-white rounded-md w-full" value="Login / Sign in" />
-                                            </div>
-
-                                            <div className="text-center">
-                                                <span className="text-slate-400 me-2">Dont have an account ?</span>
+                                                <input type="submit" className="py-2 px-5 inline-block tracking-wide border align-middle duration-500 text-base text-center bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-white rounded-md w-full" value="Login" />
                                             </div>
                                         </div>
                                     </form>
@@ -57,30 +52,17 @@ export default function Login() {
 
                         <div className="xl:w-[70%] lg:w-2/3 md:w-1/2 flex justify-center mx-6 md:my-auto my-20">
                             <div>
-                                <div className="relative">
-                                    <div className="absolute top-20 start-20 bg-indigo-600/[0.02] h-[1200px] w-[1200px] rounded-full"></div>
-                                    <div className="absolute bottom-20 -end-20 bg-indigo-600/[0.02] h-[600px] w-[600px] rounded-full"></div>
-                                </div>
+
 
                                 <div className="text-center">
                                     <div>
-
-                                        <div className="relative max-w-xl mx-auto text-start">
-                                            <div className="relative p-8 border-2 border-indigo-600 rounded-[30px] before:content-[''] before:absolute before:w-28 before:border-[6px] before:border-white dark:before:border-slate-900 before:-bottom-1 before:start-16 before:z-2 after:content-[''] after:absolute after:border-2 after:border-indigo-600 after:rounded-none after:rounded-e-[50px] after:w-20 after:h-20 after:-bottom-[80px] after:start-[60px] after:z-3 after:border-s-0 after:border-b-0">
-                                                <span className="font-semibold leading-normal">
-                                                    Launch your campaign and benefit from our expertise on designing and managing conversion centered latest Tailwind CSS html page.
-                                                </span>
-
-                                                <div className="absolute text-8xl -top-0 start-4 text-indigo-600/10 dark:text-indigo-600/20 -z-1">
-                                                    <i className="mdi mdi-format-quote-open"></i>
-                                                </div>
-                                            </div>
-
-                                            <div className="text-lg font-semibold mt-6 ms-44">
-                                                - Techwind
-                                            </div>
-                                        </div>
-                                        <p className="text-slate-400 max-w-xl mx-auto">Start working with Tailwind CSS that can provide everything you need to generate awareness, drive traffic, connect. Dummy text is text that is used in the publishing industry or by web designers to occupy the space which will later be filled with real content.</p>
+                                        <Player
+                                            src='https://lottie.host/6c54d04a-7ed0-4492-86e2-1db4102e55a1/3GEaOEUjCc.json'
+                                            className="player"
+                                            style={{ height: '500px' }}
+                                            loop
+                                            autoplay
+                                        />
                                     </div>
                                 </div>
                             </div>
