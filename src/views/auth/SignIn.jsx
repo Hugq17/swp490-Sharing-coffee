1429b937ba3271754b5c1a0cb089755ef5e59d77
hung1720 <coderlazy17@gmail.com>
2024-04-25 16:49:35 +0700

    update

--- src/views/auth/SignIn.jsx
+++ src/views/auth/SignIn.jsx
@@ -65,9 +65,6 @@ export default function SignIn() {
                                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mật khẩu</label>
                                 <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                             </div>
-                            <div class="flex items-center justify-between">
-                                {/* <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Quên mật khẩu</a> */}
-                            </div>
                             <button onClick={handleSignIn} class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Đăng nhập</button>
                         </div>
                     </div>
