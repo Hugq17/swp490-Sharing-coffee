cbba7ee785aa613f47d0c16d49c7b81825f94d7c
hung1720 <coderlazy17@gmail.com>
2024-05-01 09:30:13 +0700

    Update: tilte app

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom'
 import AuthLayout from "./layouts/auth"
 import AdminLayout from "./layouts/admin"
 import PrivateRoute from './views/auth/PrivateRoute';
+import { Helmet } from 'react-helmet';
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='auth/*' element={<AuthLayout />} />
-        <Route path='admin/*' element={<PrivateRoute element={<AdminLayout />} />} />
-        <Route path="/" element={<Navigate to="/auth/sign-in" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='auth/*' element={<AuthLayout />} />
+          <Route path='admin/*' element={<PrivateRoute element={<AdminLayout />} />} />
+          <Route path="/" element={<Navigate to="/auth/sign-in" replace />} />
+        </Routes>
+      </BrowserRouter>
+      <Helmet>
+        <title>Sharing Cafe</title>
+      </Helmet>
+    </div>
   );
 }
 
